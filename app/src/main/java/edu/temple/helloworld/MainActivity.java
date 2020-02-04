3a3b3bde27f90bb915323217aa3c9e0054bc064b
Joe Ciccone <ciccone.n.joseph@gmail.com>
2020-02-04 15:00:41 -0500

    Added functionality to change text on button press

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,12 +3,27 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView myText;
+    Button myButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myButton = findViewById(R.id.myButton);
+        myText = findViewById(R.id.textView);
+        View.OnClickListener buttonPress = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myText.setText("You have clicked the button");
+            }
+        };
+
+        myButton.setOnClickListener(buttonPress);
     }
 }
